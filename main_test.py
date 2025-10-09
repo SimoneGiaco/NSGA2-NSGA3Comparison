06af7a0bbb08a2185bc3cf89925094bec41406b4
SimoneGiaco <gcmsmn@gmail.com>
2025-10-09 18:36:12 +0000

    improving code

--- main_test.py
+++ main_test.py
@@ -3,39 +3,8 @@ from pymoo.indicators.igd import IGD
 from pymoo.indicators.hv import HV
 from pymoo.util.ref_dirs import get_reference_directions
 from src.opt import NSGA2_opt, NSGA3_opt, get_prob
+from src.utils import set_problem, set_objective
 import numpy as np
-
-
-# Auxiliary function which specifies the problem ID of n_obj from a user input
-def set_problem() -> str:
-    problem = input(
-        "Enter the problem you want to solve ('DTLZ2', 'DTLZ3', 'DTLZ6' or 'DTLZ7'): "
-    ).upper()
-    if problem in ["DTLZ2", "DTLZ3", "DTLZ6", "DTLZ7"]:
-        return problem
-    print(
-        "Invalid problem. Please enter 'DTLZ2', 'DTLZ3', 'DTLZ6' or 'DTLZ7'."
-    )  # Exception if the user input does not correspond to the ID of one of the problems.
-    return set_problem()
-
-
-# Auxiliary function which sets the value of n_obj from a user input
-def set_objective(problem) -> int:
-    try:
-        objective = int(
-            input("Enter the number of objectives (2, 3 or 4): ")
-        )  # Exception if the input from the user is not a number
-        if objective == 2 and problem in ["DTLZ6", "DTLZ7"]:
-            print(
-                "For 'DTLZ6' and 'DTLZ7' only 3 and 4 objectives are supported"
-            )  # n_obj=2 is not supported for these problems, so we treat this case separately.
-        elif objective in [2, 3, 4]:
-            return objective
-        else:
-            print("Not tested for more than 4 objectives, try another value.")
-    except ValueError as e:
-        print(e)
-    return set_objective(problem)
 
 
 # Function providing the metrics and plots for the optimized solution
